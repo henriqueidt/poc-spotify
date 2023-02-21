a3392d6f53175e2bd1c6e37cafcaa030e067fde2
Henrique Eidt <henriqueidt@gmail.com>
2023-02-20 22:56:33 -0300

    create backend

---
+++ poc-spotify-backend/src/main/java/com/poc/spotify/controllers/MusicController.java
@@ -0,0 +1,62 @@
+package com.poc.spotify.controllers;
+
+import java.util.List;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class MusicController {
+	
+	private ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+	@GetMapping("/music")
+	public ResponseEntity<ResourceRegion> streamMusic(HttpServletRequest request) throws Exception {
+		Resource musicResource = resourceLoader.getResource("classpath:com/poc/spotify/controllers/music.mp3");
+		ResourceRegion musicRegion = getResourceRegion(request, musicResource);
+		return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(musicRegion);
+				
+	}
+	
+	private ResourceRegion getResourceRegion(HttpServletRequest request, Resource resource) throws Exception {
+		long contentLength = resource.contentLength();
+		String rangeHeader = request.getHeader(HttpHeaders.RANGE);
+		
+		if(rangeHeader != null) {
+			long[] range = getRange(rangeHeader, contentLength);
+			long start = range[0];
+			long end = range[1];
+			long rangeLength = Math.min(1024 * 1024, end - start + 1);
+			return new ResourceRegion(resource, 0, rangeLength);
+		}
+		
+		throw new RuntimeException("Invalid range");
+	}
+	
+	private long[] getRange(String rangeHeader, long contentLength) {
+		rangeHeader = rangeHeader.trim().substring("bytes=".length());
+		String[] range = rangeHeader.split("-");
+		long start = Long.parseLong(range[0]);
+		
+		long end = range.length > 1 ? Long.parseLong(range[1]) : contentLength - 1;
+		end = Math.min(end, contentLength - 1);
+		
+		if(start > end) {
+			throw new IllegalArgumentException("Invalid range: " + rangeHeader);
+		}
+		
+		return new long[]{start, end};
+	}
+}
