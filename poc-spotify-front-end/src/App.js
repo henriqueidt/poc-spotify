c0b45cfa68a98c29470ab3eada42757cf53f54d1
Henrique Eidt <henriqueidt@gmail.com>
2023-03-13 22:18:12 -0300

    Create navBar

--- poc-spotify-front-end/src/App.js
+++ poc-spotify-front-end/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MusicPlayer from "./components/musicPlayer/musicPlayer";
+import { NavBar } from "./components/navBar/navBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <MusicPlayer />
     </div>
   );
---
+++ poc-spotify-front-end/src/components/navBar/navBar.jsx
@@ -0,0 +1,95 @@
+import "./navBar.css";
+import { Logo } from "../logo/logo";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+
+const links = [
+  {
+    label: "Início",
+    icon: <HomeIcon />,
+  },
+  {
+    label: "Buscar",
+    icon: <SearchIcon />,
+  },
+  {
+    label: "Sua Biblioteca",
+    icon: <LibraryBooksIcon />,
+  },
+  {
+    label: "Criar playlist",
+    icon: <AddBoxIcon />,
+  },
+  {
+    label: "Músicas Curtidas",
+    icon: <FavoriteIcon />,
+  },
+  {
+    label: "Seus Episódios",
+    icon: <BookmarkIcon />,
+  },
+];
+
+const playlists = [
+  {
+    label: "Pop",
+  },
+  {
+    label: "Rock",
+  },
+  {
+    label: "Samba",
+  },
+  {
+    label: "Pagode",
+  },
+  {
+    label: "Brasil top 100",
+  },
+  {
+    label: "Funk",
+  },
+  {
+    label: "Folk",
+  },
+  {
+    label: "Jazz",
+  },
+  {
+    label: "Liked from Radio",
+  },
+];
+
+export const NavBar = () => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <ul className="nav-bar__list">
+        {links.map((link) => (
+          <NavItem {...link} />
+        ))}
+      </ul>
+      <hr className="nav-bar__separator" />
+      <ul className="nav-bar__list">
+        {playlists.map((link) => (
+          <NavItem {...link} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const NavItem = ({ label, icon }) => {
+  return (
+    <li className="nav-item">
+      <a className="nav-item__link" href="/">
+        {icon}
+        {label}
+      </a>
+    </li>
+  );
+};
